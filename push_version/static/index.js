faffffb4cc302cff97ecd5be471a42ade00dc1ca
Tiago Loureiro <tiago.loureiro@gmail.com>
2011-10-05 21:51:12 +0300

    Fixed some script loading order issue and checked for Javascript enabled code

--- push_version/static/index.js
+++ push_version/static/index.js
@@ -1,5 +1,9 @@
-var uploadUUID = "";
+var uploadUUID = '';
 var percentDone = 0;
+
+$(function() {
+	$('#nojs').hide();
+});
 
 window.uploadStarted = function() {
 	//Display both the progress and the description form
@@ -7,3 +11,22 @@ window.uploadStarted = function() {
   	$('#progress').show();
 	$('#percent').text('Status: 0%');
 }
+
+//Deals with all the events coming from the server
+var socket = io.connect(window.location.href);
+socket.on('connect', function () {
+	
+	socket.on('sessionID', function (data) {
+		window.uploadUUID = data;
+	});
+
+	socket.on('progress', function(data) {
+		window.percentDone = parseFloat(data);
+		$('#percent').text('Status: ' + data + '%');
+	});
+	
+	socket.on('url', function(data) {
+		$('#fileLocation').html('<a href=' + data + '>Uploaded to here!</a>');
+	});
+});
+
